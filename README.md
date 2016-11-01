3354923be886f4cf1b5f98be07c02d6e507e8c67
Aditya Raisinghani <aditya.ajeet@gmail.com>
2016-11-01 10:30:22 +0400

    Base code, add license

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
 # React Table
 
-React Table is a simple, light table built on React. It makes no assumptions about your data and allows the user to define how the table reacts to pagination, filtering and sorting.
+React Table is a simple, light weight table built on React. It makes no assumptions about your data and allows the user to define how the table reacts to pagination, filtering and sorting.
+
+# Usage
+
+Create a simple table by passing the data and the columns props.
+
+## Example
+
+~~~~ 
+data: [{
+      first_name: "Eddard",
+      last_name: "Stark",
+      house: "Stark"
+    },{
+      first_name: "Arya",
+      last_name: "Stark",
+      house: "Stark"
+    },{
+      first_name: "Tywin",
+      last_name: "Lannister",
+      house: "Lannister"
+    },{
+      first_name: "Joffrey",
+      last_name: "Baratheon",
+      house: "Lannister"
+    }];
+
+columns: [{
+      name: "Name",
+      accessor: "first_name"
+    },{
+      name: "Family Name",
+      accessor: "last_name"
+    },{
+      name: "Full Name",
+      accessor: function(row)
+      {
+        return row.first_name + ' ' + row.last_name;
+      }
+    },{
+      name: "House",
+      accessor: function(row)
+      {
+        return <a href={"http://gameofthrones.wikia.com/wiki/House_" + row.house} target="_blank">{row.house}</a>;
+      }
+    }];
+~~~~~
+
+Data: An array of objects to be used by the table
+
+Column: An array of objects containing two properties- name, and accesssor. Name defines the header for the column and accessor can be the property name or a function that returns string or JSX.
+
+## Sorting
+
+React table allows you to define a handler for a sort event. The handler is passed the column object. You can handle sorting locally, or fetch the results from your server and update the data.
+
+Set the `onSort` prop with your event handler.
+
+## Filtering
+
+Similar to sorting, you define a handler on the filter event. The handler is passed the value input by the user in the filter box.
+
+Set the `onFilter` prop with your event handler.
+
+## Pagination
+
+Similar to sorting, you define a handler on the pagination event. The handler is passed the current page number, the last object displayed on the table and the new page number.
+
+Set the `onPageChange` prop with your event handler.
+
+# License
+
+MIT License
---
+++ src/react-table.js
@@ -0,0 +1,135 @@
+import React, {PropTypes} from 'react';
+
+class ReactTable extends React.Component{
+  static propTypes()
+  {
+    return {
+      data: PropTypes.array,
+      columns: PropTypes.array,
+      onPageChange: PropTypes.func,
+      onSort: PropTypes.func,
+      onFilter: PropTypes.func
+    }
+  }
+
+  constructor(props)
+  {
+    super(props);
+    // Initial setup
+    this.initialState = {};
+
+    if(props.onPageChange)
+    {
+      this.initialState.currentPage = 1;
+      this.initialState.lastObject = props.data[props.data.length-1];
+    }
+    if(props.onFilter)
+    {
+      this.initialState.filterInput = '';
+    }
+
+    this.state = this.initialState;
+  }
+
+  componentWillReceiveProps(nextProps)
+  {
+    // Might have unnecessary updates.
+    this.setState({lastObject: nextProps.data[nextProps.data.length - 1]});
+  }
+
+  sortColumn(column)
+  {
+    if(this.props.onSort)
+    {
+      this.props.onSort(column);
+    }
+  }
+
+  generateColumn(col,index)
+  {
+    return <th key={col.name} onClick={this.sortColumn.bind(this,col)}>{col.name}</th>;
+  }
+
+  showPrevious()
+  {
+    if(this.props.onPageChange)
+    {
+      this.props.onPageChange(this.state.currentPage, this.state.lastObject, this.state.currentPage -1);
+      this.setState({currentPage: this.state.currentPage-1});
+    }
+  }
+
+  showNext()
+  {
+    if(this.props.onPageChange)
+    {
+      this.props.onPageChange(this.state.currentPage, this.state.lastObject, this.state.currentPage + 1);
+      this.setState({currentPage: this.state.currentPage+1});
+    }
+  }
+
+  renderPagination()
+  {
+    return (
+      <div className="pagination">
+        {this.state.currentPage != 1? <div className="previous-page"><a onClick={this.showPrevious.bind(this)}>Previous</a></div> : ''}
+        <div className="next-page"><a onClick={this.showNext.bind(this)}>Next</a></div>
+      </div>
+    );
+  }
+
+  onFilterInputChange(e)
+  {
+    this.setState({filterInput: e.target.value});
+    if(this.props.onFilter)
+    {
+      this.props.onFilter(e.target.value);
+    }
+  }
+
+  render()
+  {
+    return (
+      <div className="react-table">
+        {this.props.onFilter? 
+          <div className="filter"><input type="text" placeholder="Search table" value={this.state.filterInput} onChange={this.onFilterInputChange.bind(this)}/></div> 
+          : ''
+        }
+        <table>
+          <thead>
+            <tr key="table-header">{this.props.columns.map(this.generateColumn, this)}</tr>
+          </thead>
+          <tbody>
+            {this.props.data.map((row,rowIndex) => 
+              {
+                return(
+                  <tr key={rowIndex}>
+                    {this.props.columns.map((col,colIndex) => 
+                      {
+                        let rowHtml;
+
+                        if(typeof col.accessor === "function")
+                        {
+                          rowHtml = <td key={col.name + rowIndex}>{col.accessor(row,rowIndex)}</td>
+                        }
+                        else
+                        {
+                          rowHtml = <td key={col.name + rowIndex}>{row[col.accessor.toString()]}</td>;
+                        }
+
+                        return rowHtml;
+                      })
+                    }
+                  </tr>  
+                );
+              }
+            )}
+          </tbody>
+        </table>
+        {this.props.onPageChange? this.renderPagination(): ''}
+      </div>
+    )
+  }
+}
+
+export default ReactTable;
